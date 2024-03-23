394f4c298b7abcea66921212fbcf50e3cab39ee5
victor@victor <victor.dmaina@gmail.com>
2024-03-23 18:32:51 +0300

    add

--- SGGenerator/index.js
+++ SGGenerator/index.js
@@ -1,17 +1,19 @@
+// Create a function to calculate and display the result based on input
 function show_result() {
+    // Get the values of HTML, CSS, and JS scores 
     let ht = document.querySelector("#ht").value;
     let css = document.querySelector("#css").value;
     let js = document.querySelector("#js").value;
-  
+  // Calculate the total marks by converting input values to numbers and adding them
     let totalMarks = parseFloat(ht) + parseFloat(css) + parseFloat(js);
     let maxTotalMarks = 100;
     let percentage = (totalMarks / maxTotalMarks) * 100;
 
     document.querySelector(".Tot").textContent = totalMarks;
-    document.querySelector(".per").textContent = percentage.toFixed(2) + "%";
+    document.querySelector(".per").textContent = Math.floor(percentage) + "%";
     
     let gradeElement = document.querySelector(".Gra")
-    
+    // Determine the grade based on the percentage and display it
     if(percentage >= 79) {
         gradeElement.innerHTML = "A";
     }else if(percentage >= 60){
@@ -24,8 +26,8 @@ function show_result() {
         gradeElement.innerHTML = "E";
     }
 
-    document.querySelector(".per").innerHTML = percentage;
-
+    
+ // Determine if the student passed or failed and display the result accordingly
     if (percentage > 40) {
         document.querySelector(".result h1").innerHTML = "PASS!";
         document.querySelector(".result h1").classList.add("pass");    
