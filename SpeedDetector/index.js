b062eb9bf21bca31f94970f3b1e31b0a8b3b3929
victor@victor <victor.dmaina@gmail.com>
2024-03-23 16:10:12 +0300

    add

--- SpeedDetector/index.js
+++ SpeedDetector/index.js
@@ -1,19 +1,30 @@
 function checkSpeed() {
+    let spdInput = document.querySelector(".data");
+    spdInput.classList.add("hide")
+
     let spd = parseInt(document.querySelector("#spd").value);
     let speedLimit = 70;
     let demeritsPer5km = 1;
     let maxDemeritPoints = 12;
 
-    if (spd <= speedLimit) {
-        document.querySelector("#result").innerText = "Ok";
-    } else {
-        const Points = Math.floor((spd - speedLimit) / 5) * demeritsPer5km;
-        if (Points >= maxDemeritPoints) {
-            document.querySelector("#result").innerText = "License suspended";
+    document.querySelector("button").classList.add("hide");
+    document.querySelector(".loader").classList.remove("hide");
+
+    setTimeout(() => {
+        if (spd <= speedLimit) {
+            document.querySelector("#result").innerText = "Ok";
         } else {
-            document.querySelector("#result").innerText = "Points: " + Points;
-           
+            const Points = Math.floor((spd - speedLimit) / 5) * demeritsPer5km;
+            if (Points >= maxDemeritPoints) {
+                document.querySelector("#result").innerText = "License suspended";
+            } else {
+                document.querySelector("#result").innerText = "Points: " + Points;
+               
+            }
         }
-    }
+        spdInput.classList.remove("hide");
+        document.querySelector("button").classList.remove("hide");
+        document.querySelector(".loader").classList.add("hide");
+        },2000);
     
 }
